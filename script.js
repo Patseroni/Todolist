6b18f91816a89f2bb41f05e95274cfb503b56f11
PatrikB <patrik.bustad@lerniastudent.se>
2024-09-24 11:17:13 +0200

    stuff

--- script.js
+++ script.js
@@ -3,27 +3,33 @@
 //when deleting text, delete from array.
 //Check if user type something and not leaving the input empty, so no empty rows get added.
 //appendChild, document.createElement(element ex "li"), .value.
-
+//firstElementChild.
+//classList ?
+//add ?
 const addTask = document.querySelector("#addTask");
 const addBtn = document.querySelector("#addBtn");
 const rmBtn = document.querySelector(".removeTaskDiv button");
 const list = document.querySelector("ul");
 const infoBox = document.querySelector("strong");
+const taskArray = [];
+
 
 //click add task-button and send the input to a new list element.
-addBtn.addEventListener("click", function() {
+addBtn.addEventListener("click", function () {
 
-    if(addTask.value.length > 0){
+    if (addTask.value.length > 0) {
         const liElement = document.createElement("li");
         liElement.innerText = addTask.value;
         list.appendChild(liElement);
         infoBox.innerText = "";
 
+        //taskArray[i] = addTask.value;
     }
-    else{
+    else {
         infoBox.innerText = "You can't add an empty task to the list";
 
     }
+
     
 
 });
